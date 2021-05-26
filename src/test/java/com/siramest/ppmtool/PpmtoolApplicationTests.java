5e5ec3dfb4fd26ca9c2eadd369701b7be1ff9697
nipat <siramest@gmail.com>
2021-05-25 22:50:17 -0400

    First commit

---
+++ src/test/java/com/siramest/ppmtool/PpmtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siramest.ppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmtoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
